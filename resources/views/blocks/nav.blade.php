1a223f1c5e8497a99be7c697c6aaee37e6d7b262
Stan <stasdre@gmail.com>
2020-09-17 10:03:49 +0300

    add our photo to nav for mobile devices

--- resources/views/blocks/nav.blade.php
+++ resources/views/blocks/nav.blade.php
@@ -1,5 +1,6 @@
 <ul class="nav__list">
     <li class="nav__item"><a class="nav__link" href="/{{app()->getLocale()}}">Home</a></li>
     <li class="nav__item"><a class="nav__link" href="/{{app()->getLocale()}}/booking">Book time</a></li>
+    <li class="nav__item nav__item_hide-large"><a class="nav__link" href="/{{app()->getLocale()}}/gallery">Our Photos</a></li>
     <li class="nav__item"><a class="nav__link" href="/{{app()->getLocale()}}/contact-us">Contact us</a></li>
 </ul>
